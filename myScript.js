db2bf6748eb95c4d6b0382bd728867ffe47fe0e4
F3licia <romeofelicia@gmail.com>
2021-04-14 15:13:05 +0200

    corretto

--- myScript.js
+++ myScript.js
@@ -1,48 +1,19 @@
 
-var primaLista = ["Bianchi", "Neri" , "Rossi" , "Verdi" , "Gialli"]
-
-var secondaLista = [ ]
-
-var cognUtente = prompt ("Inserisci il tuo cognome")
-
-while (!isNaN(cognUtente)) {
-  alert("Non valido")
-  var cognUtente = prompt ("Inserisci il tuo cognome")
-  isNaN(cognUtente)
-}
-
-primaLista.push(cognUtente)
-
-console.log(primaLista)
-
-for (var i = 0; i < primaLista.length; i++) {
-
-  secondaLista.push(primaLista[i].toLowerCase()) 
-}
-console.log(secondaLista)
-console.log(secondaLista.sort())
-console.log("Il tuo cognome si trova nella posizione n. " +  (secondaLista.indexOf(cognUtente) + 1))   /*Non funziona con toUpperCase*/
-
-
-/*----Alternativa-trovata-con-function-------------------------*/
-
-/*
-function capitalizeFirstLetter(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
 var cognomi = ["Bianchi", "Neri" , "Rossi" , "Verdi" , "Gialli"]
 
-var cognUtente = prompt ("Inserisci il tuo cognome")
+var utente = prompt ("Inserisci il tuo cognome")
 
-while (!isNaN(cognUtente)) {
+while (!isNaN(utente)) {
     alert("Non valido")
-    var cognUtente = prompt ("Inserisci il tuo cognome")
-    isNaN(cognUtente)
+    var utente = prompt ("Inserisci il tuo cognome")
+    isNaN(utente)
 }
 
-var cognomeUtente = capitalizeFirstLetter(cognUtente)
+var primaLettera = utente.slice(0, 1).toUpperCase()
+var restoParola = utente.slice(1).toLowerCase()
 
-cognomi.push(cognomeUtente)
+var cognomeUser = primaLettera + restoParola
+
+cognomi.push(cognomeUser)
 console.log(cognomi.sort());
-console.log("Il tuo cognome si trova alla posizione n. " + (cognomi.indexOf(cognomeUtente) + 1)); */
+console.log("Il tuo cognome si trova alla posizione n. " + (cognomi.indexOf(cognomeUser) + 1)); 
